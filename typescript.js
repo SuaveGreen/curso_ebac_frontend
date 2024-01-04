d3bc6aa95538b96d0e8aea796e79fe4c61761331
Pedro Caetano <86443721+SuaveGreen@users.noreply.github.com>
2024-01-04 19:04:10 -0300

    Adicionando o exercício de TypeScript

---
+++ typescript.js
@@ -0,0 +1,21 @@
+"use strict";
+// Function para dizer o nome e Olá
+function dizOla(nome) {
+    if (nome.length <= 0) {
+        return 'Digite um nome válido';
+    }
+    else {
+        return `Olá, ${nome}! Seja bem vindo!`;
+    }
+}
+// function de multiplicação
+function calculaMultiplicacao(num1, num2) {
+    if (num1 <= 0 && num2 <= 0) {
+        return `Deu ${num1}, escolha numeros maiores que ${num1}`;
+    }
+    else {
+        return num1 * num2;
+    }
+}
+console.log(dizOla('Pedro'));
+console.log(calculaMultiplicacao(173950, 25474));
---
+++ typescript.ts
@@ -0,0 +1,21 @@
+// Function para dizer o nome e Olá
+function dizOla(nome: string): string {
+    if (nome.length <= 0) {
+        return 'Digite um nome válido'
+    } else {
+        return `Olá, ${nome}! Seja bem vindo!`
+    }
+}
+
+// function de multiplicação
+function calculaMultiplicacao(num1: any, num2: any): any {
+    if (num1 <= 0 && num2 <= 0) {
+        return `Deu ${num1}, escolha numeros maiores que ${num1}`
+    } else {
+        return num1 * num2;
+    }
+}
+
+console.log(dizOla('Pedro'))
+
+console.log(calculaMultiplicacao(173950, 25474))
